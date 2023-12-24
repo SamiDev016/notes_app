5c06bb0e697630b83feb59ea081de697af14551f
samidev016 <adelim0555@gmail.com>
2023-12-24 16:38:45 +0100

    add_buttom_sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_buttom.dart';
 import 'package:noteapp/views/widgets/custom_textField.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -8,20 +9,29 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(hint: "Title"),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: "Content",
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(hint: "Title"),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: "Content",
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/custom_buttom.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:noteapp/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: KPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: const Center(
+        child: Text(
+          "Add",
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
