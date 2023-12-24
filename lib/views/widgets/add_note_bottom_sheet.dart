ce73e81abdb6910b3d2303b584d75cacc4689fed
samidev016 <adelim0555@gmail.com>
2023-12-24 16:17:28 +0100

    textfield

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,11 +1,22 @@
 
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_textField.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child:  Column(
+        children: [
+          
+          SizedBox(height: 32,),
+          CustomTextField(),
+          
+        ],
+      ),
+    );
   }
 }
